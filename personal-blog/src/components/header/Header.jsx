13f3808926c2cc872f6ee66d5eff3c24a33d026b
meglenchev <t.meglenchev@gmail.com>
2026-01-21 10:13:24 +0200

    Mobile view integration

--- personal-blog/src/components/header/Header.jsx
+++ personal-blog/src/components/header/Header.jsx
@@ -1,36 +1,50 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, NavLink } from "react-router";
 import UserContext from "../../context/UserContext.jsx";
 
 export function Header() {
     const { isAuthenticated, isAdmin } = useContext(UserContext);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+    const closeMenu = () => setIsMenuOpen(false);
 
     return (
-        <header className={isAuthenticated ? 'header logedin' : 'header'}>
+        <header className={`${isAuthenticated ? 'header logedin' : 'header'} ${isMenuOpen ? 'menu-open' : ''}`}>
             <div className="wrap">
-                <div className="content">
-                    <nav>
-                        <ul>
-                            <li><NavLink to="/">Начало</NavLink></li>
-                            <li><NavLink to="/about">За мен</NavLink></li>
-                            <li><NavLink to="/practices">Практики</NavLink></li>
-                            <li><NavLink to="/blogs">Блог</NavLink></li>
-                        </ul>
-                    </nav>
+                <button className={`mobile-menu-btn ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </button>
 
-                    <ul className="auth-nav">
-                        {isAuthenticated && <li><NavLink to="/pb-admin/logout">Изход</NavLink></li>}
-                    </ul>
-                </div>
-                {isAuthenticated && (
-                    <div className="subline">
-                        <ul>
-                            <li><NavLink to="/blogs/create">Добави блог</NavLink></li>
-                            <li><NavLink to="/practices/create">Добави практика</NavLink></li>
-                            {isAdmin && <li><NavLink to="/user/edit/settings">Настройки</NavLink></li>}
-                        </ul>
+                <div className={`menu-wrap ${isMenuOpen ? 'active' : ''}`}>
+                    <div>
+                        <div className="content">
+                            <nav>
+                                <ul>
+                                    <li><NavLink to="/" onClick={closeMenu}>Начало</NavLink></li>
+                                    <li><NavLink to="/about" onClick={closeMenu}>За мен</NavLink></li>
+                                    <li><NavLink to="/practices" onClick={closeMenu}>Практики</NavLink></li>
+                                    <li><NavLink to="/blogs" onClick={closeMenu}>Блог</NavLink></li>
+                                </ul>
+                            </nav>
+
+                            <ul className="auth-nav">
+                                {isAuthenticated && <li><NavLink to="/pb-admin/logout" onClick={closeMenu}>Изход</NavLink></li>}
+                            </ul>
+                        </div>
+                        {isAuthenticated && (
+                            <div className="subline">
+                                <ul>
+                                    <li><NavLink to="/blogs/create" onClick={closeMenu}>Добави блог</NavLink></li>
+                                    <li><NavLink to="/practices/create" onClick={closeMenu}>Добави практика</NavLink></li>
+                                    {isAdmin && <li><NavLink to="/user/edit/settings" onClick={closeMenu}>Настройки</NavLink></li>}
+                                </ul>
+                            </div>
+                        )}
                     </div>
-                )}
+                </div>
             </div>
             <h1><Link to="/"><img src="/images/strateva.png" className="logo" alt="Стратева" /></Link></h1>
         </header>
