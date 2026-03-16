619bf71dd3cbbd58d9f973356d0b41a8a810021d
meglenchev <t.meglenchev@gmail.com>
2026-03-16 11:24:16 +0200

    add image loading states and skeleton loader

--- personal-blog/src/components/blogs/Blog.jsx
+++ personal-blog/src/components/blogs/Blog.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router";
 import { useDate } from "../../hooks/useDate.js";
+import { useState } from "react";
 
 export function Blog({
     id,
@@ -9,15 +10,20 @@ export function Blog({
     date
 }) {
     const formattedDate = useDate(date)
+    const [isLoaded, setIsLoaded] = useState(false);
 
     return (
         <section className="post">
-            <img src={imageUrl} alt={title} />
+            {!isLoaded && <img src="/images/loading.svg" className="loading-image" alt="Зареждане" />}
+
+            <img src={imageUrl} alt={title} onLoad={() => setIsLoaded(true)} style={{ display: isLoaded ? 'block' : 'none' }} />
+
             <div className="content">
                 <h3>{title}</h3>
                 <p>{presentation}</p>
                 <span className="post-date">{formattedDate}</span>
             </div>
+            
             <Link to={`/blogs/${id}/details`} className="btn" title="Прочети повече">Прочети</Link>
         </section>
     )
--- personal-blog/src/components/practices/Practice.jsx
+++ personal-blog/src/components/practices/Practice.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router";
 
 export function Practice({
@@ -6,10 +7,16 @@ export function Practice({
     imageUrl,
     presentation
 }) {
+    const [isLoaded, setIsLoaded] = useState(false);
+
     return (
         <section className="post">
-            <img src={imageUrl} alt={title} />
+            {!isLoaded && <img src="/images/loading.svg" className="loading-image" alt="Зареждане" />}
+
+            <img src={imageUrl} alt={title} onLoad={() => setIsLoaded(true)} style={{ display: isLoaded ? 'block' : 'none' }} />
+
             <Link to={`/practices/${id}/details`} className="btn" title="Научи повече">Научи повече</Link>
+            
             <div className="content">
                 <h3>{title}</h3>
                 <p>{presentation}</p>
