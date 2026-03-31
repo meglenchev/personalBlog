bce155c66b55173e295deb0c73ceb2965b348a48
meglenchev <t.meglenchev@gmail.com>
2026-03-31 15:10:32 +0300

    Add Slide image loading state
    Implement isLoaded state to manage image visibility.
    Show loader until the onLoad event is triggered.
    Hide image using display: none until it's fully rendered to prevent flickering.

--- personal-blog/src/components/Home/slide/Slide.jsx
+++ personal-blog/src/components/Home/slide/Slide.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 export function Slide({ imageUrl, sliderContent }) {
+    const [isLoaded, setIsLoaded] = useState(false);
+
     return (
         <div className="slider-wrap">
+            {!isLoaded && <div className="loader"><img src="/images/loading.svg" alt="Зареждане" /></div>}
             <div className="slider-content">
                 <h3>{sliderContent}</h3>
             </div>
-            <img src={imageUrl} alt="" />
+            <img src={imageUrl} alt="" onLoad={() => setIsLoaded(true)} style={{ display: isLoaded ? 'block' : 'none' }} />
         </div>
     )
 }
